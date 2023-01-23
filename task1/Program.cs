16a4ffa467bd5236e43eb8c338a7ab45ca19eaa6
nadezda staselovich <staselovichn@mail.ru>
2023-01-23 17:07:38 +0200

    add commit

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+
+Console.WriteLine("Введите пятизначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int num1 = number / 10000;
+int num2 = (number / 1000) % 10;
+int num3 = (number % 100) / 10;
+int num4 = number % 10;
+
+if (num1 == num4 && num2 == num3)
+{
+  System.Console.WriteLine("да");
+}
+else
+{
+  System.Console.WriteLine("нет");
+}
